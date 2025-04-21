52308f6924ec8c3e9dafb8e6c9808f96c1bd2e8d
Jeremy <155750978+defiantdevs@users.noreply.github.com>
2025-04-20 20:04:57 -0500

    Update Errors

--- ajax/ajax_recurring_ticket_edit.php
+++ ajax/ajax_recurring_ticket_edit.php
@@ -1,4 +1,8 @@
 <?php
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once '../includes/ajax_header.php';
 
--- post/user/ticket.php
+++ post/user/ticket.php
@@ -1,5 +1,10 @@
 <?php
 
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 /*
  * ITFlow - GET/POST request handler for client tickets
  */
